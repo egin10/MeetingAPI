8d8f4a6ee661fe3709f657dc3f9a6728da60476d
egin10 <egin066@gmail.com>
2018-11-12 09:30:57 +0800

    add user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class AuthController extends Controller
 {
     /**
@@ -13,7 +15,42 @@ class AuthController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request) {
-        return 'it works!';
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+        ]);
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        
+        $user = new User([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt($password)
+        ]);
+
+        if ($user->save()) {
+            $user->signin = [
+                'href' => 'api/v1/user/signin',
+                'method' => 'POST',
+                'params' => 'email, password'
+            ];
+
+            $response = [
+                'msg' => 'User created',
+                'user' => $user
+            ];
+
+            return response()->json($response, 201);
+        }
+        
+        $response = [
+            'msg' => 'An Error occured'
+        ];
+
+        return response()->json($response, 404);
     }
 
     /**
