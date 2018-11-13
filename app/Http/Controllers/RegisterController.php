29c7394c12b57cfcedd3125cd96e0616ef6feb7b
egin10 <egin066@gmail.com>
2018-11-13 11:02:07 +0800

    add registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+use App\Meeting;
+
 class RegisterController extends Controller
 {
     /**
@@ -14,7 +17,27 @@ class RegisterController extends Controller
      */
     public function store(Request $request)
     {
-        return 'it works!';
+        $this->validate([
+            'meeting_id' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        $meeting_id = $request->input('meeting_id');
+        $user_id = $request->input('user_id');
+
+        $meeting = Meeting::findOrFail($meeting_id);
+        $user = User::findOrFail($user_id);
+
+        $message = [
+            'msg' => 'User is already registered for meeting',
+            'user' => $user,
+            'meeting' => $meeting,
+            'unregister' => [
+                'href' => 'api/v1/meeting/registration' . $meeting_id,
+                'method' => 'DELETE'
+            ]
+        ];
+        
     }
 
     /**
